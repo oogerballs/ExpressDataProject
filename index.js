1b016919007dcfba2be40252a792affcf7ae9562
oogerballs <36011013+oogerballs@users.noreply.github.com>
2018-12-10 09:21:33 -0700

    the files I changed

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+  pug = require('pug'),
+  path = require('path'),
+  route = require('./routes/routes.js'),
+  bodyParser = require('body-parser');
+
+
+var app = express();
+
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+var urlencodedParser = bodyParser.urlencoded({
+  extended: true
+})
+
+app.get('/', route.index);
+app.get('/create', route.create);
+app.get('/edit/:id', route.edit);
+app.get('/details/:id', route.details);
+app.post('/create', urlencodedParser, route.createPerson);
+app.post('/edit/:id', urlencodedParser, route.editPerson);
+app.get('/delete/:id', route.delete);
+app.get('/login', route.login);
+app.post('/login', urlencodedParser, route.loginPerson);
+
+
+app.listen(3000);
---
+++ routes.js
@@ -0,0 +1,111 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/data');
+
+var mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error:'));
+mdb.once('open', function (callback) {
+
+});
+
+var personSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  age: String,
+  email: String,
+  animal: String
+});
+
+var Person = mongoose.model('People_Collection', personSchema);
+
+exports.index = function (req, res) {
+  Person.find(function (err, person) {
+    if (err) return console.error(err);
+    res.render('index', {
+      title: 'People List',
+      people: person
+    });
+  });
+};
+
+exports.create = function (req, res) {
+  res.render('create', {
+      title: 'Register'
+  });
+};
+
+exports.createPerson = function (req, res) {
+  var person = new Person({
+    username: req.body.username,
+    password: req.body.password,
+    age: req.body.age,
+    email: req.body.email,
+    animal: req.body.Animal
+  });
+  person.save(function (err, person) {
+    if (err) return console.error(err);
+    console.log(req.body.username + ' added');
+  });
+  res.redirect('/');
+};
+
+exports.edit = function (req, res) {
+  Person.findById(req.params.id, function (err, person) {
+    if (err) return console.error(err);
+    res.render('edit', {
+      title: 'Edit Person',
+      person: person
+    });
+  });
+};
+
+exports.editPerson = function (req, res) {
+  Person.findById(req.params.id, function (err, person) {
+    if (err) return console.error(err);
+    person.username = req.body.username;
+    person.password = req.body.password;
+    person.age = req.body.age;
+    person.email = req.body.email;
+    person.animal = req.body.animal;
+    person.save(function (err, person) {
+      if (err) return console.error(err);
+      console.log(req.body.username + ' updated');
+    });
+  });
+  res.redirect('/');
+
+};
+
+exports.delete = function (req, res) {
+  Person.findByIdAndRemove(req.params.id, function (err, person) {
+    if (err) return console.error(err);
+    res.redirect('/');
+  });
+};
+
+exports.details = function (req, res) {
+  Person.findById(req.params.id, function (err, person) {
+    if (err) return console.error(err);
+    res.render('details', {
+      title: person.username + "'s Details",
+      person: person
+    });
+  });
+};
+
+exports.loginPerson = function (req, res) {
+  Person.findOne({username: req.body.username , password: req.body.password}, function (err, person) {
+      if(person == null || person == undefined){
+      req.body.response = "Username or Password is incorrect"
+      }else{
+        var personID = person.id;
+        res.redirect('/details/'+personID);
+      }
+  });
+};
+
+exports.login = function (req, res) {
+  res.render('login', {
+      title: 'Login Page'
+  });
+};
